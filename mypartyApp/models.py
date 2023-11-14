c7b3cb0585e138c113176160d5d5e46e93cfad7c
Guilherme Pereira <pgguilherme337@gmail.com>
2023-11-14 16:58:13 -0300

    adicao de algumas coisinhas

--- mypartyApp/models.py
+++ mypartyApp/models.py
@@ -5,11 +5,21 @@ from django.db import models
 class clients(models.Model):
     clientId = models.AutoField(primary_key=True)
     clientName = models.CharField(max_length=30)
+    clientEmail = models.EmailField()
+    clientNumber = models.CharField()
+    clientPasssword = models.CharField()
+    clientcpf = models.CharField()
+    
 
 
 
 class events(models.Model):
     eventId = models.AutoField(primary_key=True)
     eventName = models.CharField(max_length=500)
-    
-
+    eventBeggin =  models.CharField(max_length=500)
+    eventEnd =  models.CharField(max_length=500)
+    qtd_max = models.CharField(max_length=500)
+    cidade  = models.CharField(max_length=500)
+    estado = models.CharField(max_length=500)
+    cep = models.CharField(max_length=500)
+    produtor = models.CharField(max_length=500)
